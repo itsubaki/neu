e535aee7d026d5ec1df3a5c9e9bf0f97429c5d0e
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-10-05 14:20:04 +0000

    Add step, ReLU function

--- perceptron/activation.go
+++ perceptron/activation.go
@@ -2,6 +2,18 @@ package perceptron
 
 import "math"
 
+func Step(x float64) float64 {
+	if x > 0 {
+		return 1
+	}
+
+	return 0
+}
+
 func Sigmoid(x float64) float64 {
 	return 1 / (1 + math.Exp(-x))
+}
+
+func ReLU(x float64) float64 {
+	return math.Max(0, x)
 }
