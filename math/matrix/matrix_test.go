a29aa92973bec43c0077532fb7f5ccb989548af5
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-10-09 14:38:29 +0000

    Add test

--- math/matrix/matrix_test.go
+++ math/matrix/matrix_test.go
@@ -2,6 +2,7 @@ package matrix_test
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/itsubaki/neu/math/matrix"
 )
@@ -58,6 +59,80 @@ func ExampleMatrix_Add() {
 
 }
 
+func ExampleMatrix_Sub() {
+	A := matrix.New(
+		[]float64{1, 2},
+		[]float64{3, 4},
+	)
+
+	B := matrix.New(
+		[]float64{5, 6},
+		[]float64{7, 8},
+	)
+
+	for _, r := range A.Sub(B) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [-4 -4]
+	// [-4 -4]
+
+}
+
+func ExampleMatrix_Mul() {
+	A := matrix.New(
+		[]float64{1, 2},
+		[]float64{3, 4},
+	)
+
+	B := matrix.New(
+		[]float64{5, 6},
+		[]float64{7, 8},
+	)
+
+	for _, r := range A.Mul(B) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [5 12]
+	// [21 32]
+
+}
+
+func ExampleMatrix_Addf64() {
+	A := matrix.New(
+		[]float64{1, 2},
+		[]float64{3, 4},
+	)
+
+	for _, r := range A.Addf64(3.0) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [4 5]
+	// [6 7]
+
+}
+
+func ExampleMatrix_Mulf64() {
+	A := matrix.New(
+		[]float64{1, 2},
+		[]float64{3, 4},
+	)
+
+	for _, r := range A.Mulf64(3.0) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [3 6]
+	// [9 12]
+
+}
+
 func ExampleMatrix_T() {
 	A := matrix.New(
 		[]float64{1, 2, 3},
@@ -72,6 +147,62 @@ func ExampleMatrix_T() {
 	// [1 4]
 	// [2 5]
 	// [3 6]
+}
+
+func ExampleZero() {
+	for _, r := range matrix.Zero(2, 3) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [0 0 0]
+	// [0 0 0]
+
+}
+
+func ExampleRand() {
+	rand.Seed(1)
+	for _, r := range matrix.Rand(2, 3) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [-1.233758177597947 -0.12634751070237293 -0.5209945711531503]
+	// [2.28571911769958 0.3228052526115799 0.5900672875996937]
+
+}
+
+func ExampleSigmoid() {
+	x := matrix.New([]float64{0, 1}, []float64{2, 3})
+	for _, r := range matrix.Sigmoid(x) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [0.5 0.7310585786300049]
+	// [0.8807970779778823 0.9525741268224334]
+}
+
+func ExampleIdentity() {
+	x := matrix.New([]float64{0, 1}, []float64{2, 3})
+	for _, r := range matrix.Identity(x) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [0 1]
+	// [2 3]
+}
+
+func ExampleMask() {
+	x := matrix.New([]float64{0, 1}, []float64{2, 3})
+	for _, r := range matrix.Mask(x, [][]bool{{true, false}, {true, false}}) {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [0 1]
+	// [0 3]
 }
 
 func ExampleSumAxis1() {
