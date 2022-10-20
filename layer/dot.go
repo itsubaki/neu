2453170ba9604760f6dbf43f6cb70cc4bf9908f8
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-10-20 12:29:18 +0000

    Add matrix dot layer

---
+++ layer/dot.go
@@ -0,0 +1,20 @@
+package layer
+
+import "github.com/itsubaki/neu/math/matrix"
+
+type Dot struct {
+	x matrix.Matrix
+	w matrix.Matrix
+}
+
+func (l *Dot) Forward(x, w matrix.Matrix) matrix.Matrix {
+	l.x = x
+	l.w = w
+	return matrix.Dot(l.x, l.w)
+}
+
+func (l *Dot) Backward(dout matrix.Matrix) (matrix.Matrix, matrix.Matrix) {
+	dx := matrix.Dot(dout, l.w.T())
+	dy := matrix.Dot(l.x.T(), dout)
+	return dx, dy
+}
---
+++ layer/dot_test.go
@@ -0,0 +1,46 @@
+package layer_test
+
+import (
+	"fmt"
+
+	"github.com/itsubaki/neu/layer"
+	"github.com/itsubaki/neu/math/matrix"
+)
+
+func ExampleDot() {
+	A := matrix.New(
+		[]float64{1, 2},
+		[]float64{3, 4},
+	)
+
+	B := matrix.New(
+		[]float64{5, 6},
+		[]float64{7, 8},
+	)
+
+	dot := &layer.Dot{}
+	for _, r := range dot.Forward(A, B) {
+		fmt.Println(r)
+	}
+	fmt.Println()
+
+	dx, dy := dot.Backward(matrix.New([]float64{1, 0}, []float64{0, 1}))
+	for _, r := range dx {
+		fmt.Println(r)
+	}
+	fmt.Println()
+
+	for _, r := range dy {
+		fmt.Println(r)
+	}
+
+	// Output:
+	// [19 22]
+	// [43 50]
+	//
+	// [5 7]
+	// [6 8]
+	//
+	// [1 3]
+	// [2 4]
+}
