83446fcf087ce4476345b30c9b16c6e8ef66516e
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-10-26 11:17:55 +0000

    Update test

--- layer/dropout_test.go
+++ layer/dropout_test.go
@@ -12,16 +12,39 @@ func ExampleDropout() {
 	x := matrix.New([]float64{1.0, -0.5}, []float64{-2.0, 3.0})
 
 	rand.Seed(1)
-	drop := layer.Dropout{Ratio: 0.5}
+	drop := layer.Dropout{Ratio: 0}
 	fmt.Println(drop.Forward(x, nil, layer.Opts{Train: true}))
 	fmt.Println(drop.Backward(x))
+	fmt.Println(drop.Forward(x, nil, layer.Opts{Train: false}))
+	fmt.Println(drop.Backward(x))
+	fmt.Println()
 
+	drop.Ratio = 1.0
+	fmt.Println(drop.Forward(x, nil, layer.Opts{Train: true}))
+	fmt.Println(drop.Backward(x))
+	fmt.Println(drop.Forward(x, nil, layer.Opts{Train: false}))
+	fmt.Println(drop.Backward(x))
+	fmt.Println()
+
+	drop.Ratio = 0.5
+	fmt.Println(drop.Forward(x, nil, layer.Opts{Train: true}))
+	fmt.Println(drop.Backward(x))
 	fmt.Println(drop.Forward(x, nil, layer.Opts{Train: false}))
 	fmt.Println(drop.Backward(x))
 
 	// Output:
-	// [[1 -0.5] [-2 0]]
-	// [[1 -0.5] [-2 0]] []
+	// [[1 -0.5] [-2 3]]
+	// [[1 -0.5] [-2 3]] []
+	// [[1 -0.5] [-2 3]]
+	// [[1 -0.5] [-2 3]] []
+	//
+	// [[0 -0] [-0 0]]
+	// [[0 -0] [-0 0]] []
+	// [[0 -0] [-0 0]]
+	// [[0 -0] [-0 0]] []
+	//
+	// [[0 -0] [-2 3]]
+	// [[0 -0] [-2 3]] []
 	// [[0.5 -0.25] [-1 1.5]]
-	// [[1 -0.5] [-2 0]] []
+	// [[0 -0] [-2 3]] []
 }
