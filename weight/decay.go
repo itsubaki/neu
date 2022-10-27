6b36cfa985feeb7fb8178fc88314ee040cbde80c
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-10-27 13:17:04 +0000

    Add decay

---
+++ weight/decay.go
@@ -0,0 +1,17 @@
+package weight
+
+import "github.com/itsubaki/neu/math/matrix"
+
+func Decay(lambda float64) func(params, grads [][]matrix.Matrix) [][]matrix.Matrix {
+	return func(params, grads [][]matrix.Matrix) [][]matrix.Matrix {
+		out := make([][]matrix.Matrix, len(params))
+		for i := range params { // layer
+			out[i] = make([]matrix.Matrix, len(params[i]))
+			for j := range params[i] { // W, B, ...
+				out[i][j] = grads[i][j].Add(params[i][j].MulC(lambda)) // grad = grad + lambda * param
+			}
+		}
+
+		return out
+	}
+}
---
+++ weight/decay_test.go
@@ -0,0 +1,26 @@
+package weight_test
+
+import (
+	"fmt"
+
+	"github.com/itsubaki/neu/math/matrix"
+	"github.com/itsubaki/neu/weight"
+)
+
+func ExampleDecay() {
+	params := append(make([][]matrix.Matrix, 0), []matrix.Matrix{
+		matrix.New([]float64{1, 2, 3}, []float64{4, 5, 6}),
+	})
+	grads := append(make([][]matrix.Matrix, 0), []matrix.Matrix{
+		matrix.New([]float64{2, 4, 6}, []float64{8, 10, 12}),
+	})
+
+	fmt.Println(weight.Decay(0.0)(params, grads))
+	fmt.Println(weight.Decay(0.5)(params, grads))
+	fmt.Println(weight.Decay(1.0)(params, grads))
+
+	// Output:
+	// [[[[2 4 6] [8 10 12]]]]
+	// [[[[2.5 5 7.5] [10 12.5 15]]]]
+	// [[[[3 6 9] [12 15 18]]]]
+}
