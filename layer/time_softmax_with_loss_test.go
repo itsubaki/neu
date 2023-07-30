fe44f0647cd1e42c541f11a980edc4161a9b96de
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-07-30 12:42:52 +0000

    Add test

--- layer/time_softmax_with_loss_test.go
+++ layer/time_softmax_with_loss_test.go
@@ -1,1 +1,59 @@
 package layer_test
+
+import (
+	"fmt"
+
+	"github.com/itsubaki/neu/layer"
+	"github.com/itsubaki/neu/math/matrix"
+)
+
+func ExampleTimeSoftmaxWithLoss() {
+	l := &layer.TimeSoftmaxWithLoss{}
+
+	// forward
+	xs := []matrix.Matrix{matrix.New(
+		[]float64{0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0},
+		[]float64{0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0},
+	)}
+	ts := []matrix.Matrix{matrix.New(
+		[]float64{0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+		[]float64{0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+	)}
+	loss := l.Forward(xs, ts)
+	fmt.Println(loss)
+
+	// backward
+	dout := []matrix.Matrix{matrix.New([]float64{1})}
+	dx := l.Backward(dout)
+	for _, m := range dx {
+		for _, r := range m {
+			fmt.Println(r)
+		}
+	}
+
+	// Output:
+	// [[[2.0694934853340516]]]
+	// [0.04916164744936827 0.04676400561076958 -0.41894614614756587 0.044483298144480495 0.04676400561076958 0.04916164744936827 0.044483298144480495 0.04916164744936827 0.044483298144480495 0.044483298144480495]
+	// [0.04916164744936827 0.04676400561076958 -0.45083835255063176 0.044483298144480495 0.04676400561076958 0.04916164744936827 0.044483298144480495 0.08105385385243416 0.044483298144480495 0.044483298144480495]
+
+}
+
+func ExampleTimeSoftmaxWithLoss_Params() {
+	l := &layer.TimeSoftmaxWithLoss{}
+	l.SetParams(make([]matrix.Matrix, 0)...)
+
+	fmt.Println(l.Params())
+	fmt.Println(l.Grads())
+
+	// Output:
+	// []
+	// []
+}
+
+func ExampleTimeSoftmaxWithLoss_state() {
+	l := &layer.TimeSoftmaxWithLoss{}
+	l.SetState(matrix.New())
+	l.ResetState()
+
+	// Output:
+}
