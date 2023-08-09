b2b6610686de701cf94a6c6465bb7cec6ca488b4
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-08-09 17:52:31 +0900

    Update model

--- model/model.go
+++ model/model.go
@@ -18,6 +18,7 @@ var (
 	_ Layer = (*layer.Dropout)(nil)
 	_ Layer = (*layer.EmbeddingDot)(nil)
 	_ Layer = (*layer.Embedding)(nil)
+ _ Layer = (*layer.LSTM)(nil)
 	_ Layer = (*layer.Mul)(nil)
 	_ Layer = (*layer.ReLU)(nil)
 	_ Layer = (*layer.RNN)(nil)
