83ff1a333c50c670fac770a237ccdf983eaecfe5
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-08-20 23:49:56 +0900

    Fix test

--- model/rnnlm_gen_test.go
+++ model/rnnlm_gen_test.go
@@ -21,10 +21,12 @@ func ExampleRNNLMGen() {
 		DropoutRatio: 0.5,
 	}, s)
 
-	fmt.Println(m.Generate(0, []int{86, 28, 37}, 10))
+	fmt.Println(m.Generate(0, []int{}, 10))
+	fmt.Println(m.Generate(0, []int{6, 99, 42}, 10))
 
 	// Output:
-	// [30 29 45 20 58 3 68 43 31 72]
+	// [6 99 42 79 43 79 13 96 46 92]
+	// [67 12 97 81 66 55 64 25 39 90]
 }
 
 func ExampleRNNLMGen_Summary() {
@@ -47,9 +49,9 @@ func ExampleRNNLMGen_Summary() {
 	// *model.RNNLMGen
 	//  0: *layer.TimeEmbedding: W(100, 100): 10000
 	//  1: *layer.TimeDropout: Ratio(0.5)
-	//  2: *layer.TimeLSTM: Wx(100, 400), Wh(100, 400), B(1, 400): 80400
+	//  2: *layer.TimeGRU: Wx(100, 300), Wh(100, 300), B(1, 300): 60300
 	//  3: *layer.TimeDropout: Ratio(0.5)
-	//  4: *layer.TimeLSTM: Wx(100, 400), Wh(100, 400), B(1, 400): 80400
+	//  4: *layer.TimeGRU: Wx(100, 300), Wh(100, 300), B(1, 300): 60300
 	//  5: *layer.TimeDropout: Ratio(0.5)
 	//  6: *layer.TimeAffine: W(100, 100), B(1, 100): 10100
 	//  7: *layer.TimeSoftmaxWithLoss
