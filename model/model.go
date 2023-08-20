836be391146836759c91e12aed31f269e6e16584
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-08-20 21:43:27 +0900

    Add GRU interface validation

--- model/model.go
+++ model/model.go
@@ -18,6 +18,7 @@ var (
 	_ Layer = (*layer.Dropout)(nil)
 	_ Layer = (*layer.EmbeddingDot)(nil)
 	_ Layer = (*layer.Embedding)(nil)
+	_ Layer = (*layer.GRU)(nil)
 	_ Layer = (*layer.Mul)(nil)
 	_ Layer = (*layer.NegativeSamplingLoss)(nil)
 	_ Layer = (*layer.ReLU)(nil)
