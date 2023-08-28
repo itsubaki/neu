6ea7434fed55fafb97d401fc7c80522b45f02c79
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-08-28 23:39:35 +0900

    Update TemporalDiffAgent

--- agent/temporal_diff.go
+++ agent/temporal_diff.go
@@ -1,1 +1,39 @@
 package agent
+
+import (
+	"math/rand"
+
+	"github.com/itsubaki/neu/math/vector"
+)
+
+type TemporalDiffAgent struct {
+	Gamma         float64
+	Alpha         float64
+	ActionSize    int
+	RandomActions RandomActions
+	Pi            map[string]RandomActions
+	V             map[string]float64
+	Source        rand.Source
+}
+
+func (a *TemporalDiffAgent) GetAction(state string) int {
+	if _, ok := a.Pi[state]; !ok {
+		a.Pi[state] = a.RandomActions
+	}
+	probs := make([]float64, a.ActionSize)
+	for i, p := range a.Pi[state] {
+		probs[i] = p
+	}
+
+	return vector.Choice(probs, a.Source)
+}
+
+func (a *TemporalDiffAgent) Eval(state string, reward float64, next string, done bool) {
+	var nextV float64
+	if !done {
+		nextV = a.V[next]
+	}
+
+	target := reward + a.Gamma*nextV
+	a.V[state] += a.Alpha * (target - a.V[state])
+}
--- agent/temporal_diff_test.go
+++ agent/temporal_diff_test.go
@@ -1,1 +1,62 @@
 package agent_test
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+
+	"github.com/itsubaki/neu/agent"
+	"github.com/itsubaki/neu/agent/env"
+)
+
+func ExampleTemporalDiffAgent() {
+	env := env.NewGridWorld()
+	a := agent.TemporalDiffAgent{
+		Gamma:         0.9,
+		Alpha:         0.1,
+		ActionSize:    4,
+		RandomActions: agent.RandomActions{0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25},
+		Pi:            make(map[string]agent.RandomActions),
+		V:             make(map[string]float64),
+		Source:        rand.NewSource(1),
+	}
+
+	episodes := 1000
+	for i := 0; i < episodes; i++ {
+		state := env.Reset()
+
+		for {
+			action := a.GetAction(state.String())
+			next, reward, done := env.Step(action)
+
+			a.Eval(state.String(), reward, next.String(), done)
+			if done {
+				break
+			}
+
+			state = next
+		}
+	}
+
+	keys := make([]string, 0)
+	for k := range a.V {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Printf("%s: %.2f\n", k, a.V[k])
+	}
+
+	// Output:
+	// (0, 0): 0.04
+	// (0, 1): 0.14
+	// (0, 2): 0.27
+	// (1, 0): -0.02
+	// (1, 2): -1.06
+	// (1, 3): -0.68
+	// (2, 0): -0.08
+	// (2, 1): -0.17
+	// (2, 2): -0.46
+	// (2, 3): -0.85
+}
