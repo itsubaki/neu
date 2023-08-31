ca46c17c7d766f05d906cd770ae4ca5a392b446d
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-01 01:55:53 +0900

    Add qlearning_nn

---
+++ agent/qlearning_nn.go
@@ -0,0 +1,48 @@
+package agent
+
+import (
+	"log"
+	"math/rand"
+
+	"github.com/itsubaki/neu/math/matrix"
+	"github.com/itsubaki/neu/math/vector"
+	"github.com/itsubaki/neu/model"
+	"github.com/itsubaki/neu/optimizer"
+)
+
+type QLearningAgentNN struct {
+	Gamma      float64
+	Epsilon    float64
+	ActionSize int
+	Q          *model.QNet
+	Optimizer  *optimizer.SGD
+	Source     rand.Source
+}
+
+func (a *QLearningAgentNN) GetAction(state [][]float64) int {
+	rng := rand.New(a.Source)
+	if a.Epsilon > rng.Float64() {
+		return rng.Intn(a.ActionSize)
+	}
+
+	qs := a.Q.Forward(matrix.New(state...))
+	return vector.Argmax(qs[0])
+}
+
+func (a *QLearningAgentNN) Update(state [][]float64, action int, reward float64, next [][]float64, done bool) matrix.Matrix {
+	var nextq float64
+	if !done {
+		nextqs := a.Q.Forward(matrix.New(next...))
+		log.Println(nextqs)
+		nextq = vector.Max(nextqs[0])
+	}
+
+	target := reward + a.Gamma*nextq
+	qs := a.Q.Forward(matrix.New(state...))
+	q := qs[0][action]
+	loss := a.Q.MeanSquaredError(matrix.New([]float64{target}), matrix.New([]float64{q}))
+
+	a.Q.Backward()
+	a.Optimizer.Update(a.Q)
+	return loss
+}
---
+++ agent/qlearning_nn_test.go
@@ -0,0 +1,64 @@
+package agent_test
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/itsubaki/neu/agent"
+	"github.com/itsubaki/neu/agent/env"
+	"github.com/itsubaki/neu/model"
+	"github.com/itsubaki/neu/optimizer"
+	"github.com/itsubaki/neu/weight"
+)
+
+func ExampleQLearningAgentNN() {
+	env := env.NewGridWorld()
+	a := agent.QLearningAgentNN{
+		Gamma:      0.9,
+		Epsilon:    0.1,
+		ActionSize: 4,
+		Q: model.NewQNet(&model.QNetConfig{
+			InputSize:  12,
+			OutputSize: 4,
+			HiddenSize: 100,
+			WeightInit: weight.Xavier,
+		}, rand.NewSource(1)),
+		Optimizer: &optimizer.SGD{LearningRate: 0.001},
+		Source:    rand.NewSource(1),
+	}
+
+	episodes := 10
+	for i := 0; i < episodes; i++ {
+		state := env.OneHot(env.Reset())
+		var totalLoss float64
+		var count int
+
+		for {
+			action := a.GetAction(state)
+			next, reward, done := env.Step(action)
+			nextoh := env.OneHot(next)
+			loss := a.Update(state, action, reward, nextoh, done)
+			totalLoss += loss[0][0]
+			count++
+
+			if done {
+				break
+			}
+
+			state = nextoh
+		}
+
+		fmt.Println(i, totalLoss/float64(count))
+	}
+
+	for i := 0; i < 12; i++ {
+		s := env.State()
+		q := a.Q.Forward(env.OneHot(s))
+		for j := 0; j < 4; j++ {
+			fmt.Printf("%s %-6s: %.4f\n", s, env.ActionMeaning[j], q[0][j])
+		}
+	}
+
+	// Output:
+
+}
