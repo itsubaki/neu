44b5afa4054998f3cd12e9bec0a34ad4d2ff4d61
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-03 01:34:59 +0900

    Update test

--- model/qnet_test.go
+++ model/qnet_test.go
@@ -31,6 +31,15 @@ func ExampleQNet() {
 	fmt.Printf("%.8f\n", m.Loss(target, q))
 	fmt.Printf("%.8f\n", m.Backward())
 
+	params, grads := m.Params(), m.Grads()
+	for i, p := range params {
+		for j := range p {
+			a, b := params[i][j].Dim()
+			c, d := grads[i][j].Dim()
+			fmt.Printf("p(%3v, %3v), g(%3v,%3v)\n", a, b, c, d)
+		}
+	}
+
 	// Output:
 	// *model.QNet
 	//  0: *layer.Affine: W(12, 100), B(1, 100): 1300
@@ -40,4 +49,8 @@ func ExampleQNet() {
 	// [[0.00015980 -0.00057628 0.00159733 0.00016082]], [[0.00159733]]
 	// [[0.99680788]]
 	// [[0.00035037 -0.00028802 0.00124664 -0.00074836 0.00394256 -0.00254541 -0.00054185 -0.00220917 0.00031910 -0.00210713 -0.00066213 -0.00062087]]
+	// p( 12, 100), g( 12,100)
+	// p(  1, 100), g(  1,100)
+	// p(100,   4), g(100,  1)
+	// p(  1,   4), g(  1,  1)
 }
