dec78931dbb67cef7e093ea323d30a188bfaaad4
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-09-10 11:36:55 +0900

    Rename receiver name

--- agent/deque.go
+++ agent/deque.go
@@ -12,22 +12,22 @@ func NewDeque[T any](size int) *Deque[T] {
 	}
 }
 
-func (d *Deque[T]) Len() int {
-	return len(d.data)
+func (q *Deque[T]) Len() int {
+	return len(q.data)
 }
 
-func (d *Deque[T]) Size() int {
-	return d.size
+func (q *Deque[T]) Size() int {
+	return q.size
 }
 
-func (d *Deque[T]) Add(m T) {
-	if len(d.data) == d.size {
-		d.data = d.data[1:]
+func (q *Deque[T]) Add(m T) {
+	if len(q.data) == q.size {
+		q.data = q.data[1:]
 	}
 
-	d.data = append(d.data, m)
+	q.data = append(q.data, m)
 }
 
-func (d *Deque[T]) Get(i int) T {
-	return d.data[i]
+func (q *Deque[T]) Get(i int) T {
+	return q.data[i]
 }
